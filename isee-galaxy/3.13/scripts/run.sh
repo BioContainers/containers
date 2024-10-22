a9dc92cb9fbfd92dda08a890392375250ebde500
Facundo1224 <facu.mercado1224@gmail.com>
2024-10-22 19:06:21 -0300

    removing old files 8

--- isee-galaxy/3.13/scripts/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Run isee by passing templated R script to Rscript
-# WDIR: /import
-
-# Automatically kill container when traffic stops
-/scripts/monitor_traffic.sh &
-
-Rscript --vanilla $1
